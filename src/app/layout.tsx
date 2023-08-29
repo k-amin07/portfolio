14c6e428d35d2516459ce9903f37a88107a6af12
Khizar Amin <khizaramin95@gmail.com>
2023-08-30 04:18:04 +0500

    added pwa config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,46 @@ import Navbar from './components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
+const APP_NAME = "Portfolio";
+const APP_DEFAULT_TITLE = "Khizar Amin";
+const APP_TITLE_TEMPLATE = "%s - PWA App";
+const APP_DESCRIPTION = "Portfolio created using NextJS";
+
 export const metadata: Metadata = {
-  title: 'Khizar Amin',
-  description: 'Portfolio created using NextJS',
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  themeColor: "#FFFFFF",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
 }
 
 export default function RootLayout({
