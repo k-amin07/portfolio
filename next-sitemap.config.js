aa4b543fec9b24372ed8d9ee4aa9de8cc22287c4
Khizar Amin <kamin@bridgelinxpk.com>
2023-09-08 16:57:45 +0500

    static sitemap url

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'http://localhost:3000',
+    siteUrl: "https://www.khizaramin.com",
     generateRobotsTxt: true,
     generateIndexSitemap: false,
 }
